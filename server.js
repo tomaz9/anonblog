062410dd2e25ebf9130ce40c9bbf5ea2c6b264f6
Tomaz Vesel <tomaz.vesel95@gmail.com>
2017-05-19 08:33:19 +0000

    Implemented GET/POST functions

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 var express = require('express');
 var server  = express();
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var mongoose = require( 'mongoose' );
+var Schema   = mongoose.Schema;
 server.set('view engine', 'ejs');
 server.use(express.static('public'));
+server.use(bodyParser.urlencoded({'extended':'true'}));
+server.use(bodyParser.json());
+
+var Blogpost = new Schema({
+    title      : String,
+    content    : String,
+    updated_at : Date
+});
+ 
+mongoose.model('Blogpost', Blogpost);
+mongoose.connect('mongodb://localhost/data');
 
 server.get('/', function (req, res) {
-    res.render('index', {});
+    var mongoose = require('mongoose');
+    var Blogpost = mongoose.model('Blogpost');
+    Blogpost.find( function (err, blogposts, count){
+    if (!err)
+        res.render('index', {
+        blogposts: blogposts
+        });
+    else 
+        console.log("Error while getting posts");
+  });
+});
+
+server.post('/post', function (req, res) {
+    var mongoose = require('mongoose');
+    var Blogpost = mongoose.model('Blogpost');
+    console.log(req.body.title);
+    console.log(req.body.content);
+    new Blogpost({
+        title : req.body.title,
+        content : req.body.content,
+        updated_at : Date.now()
+    }).save( function(err, blogpost, count){
+        if (!err)
+            res.redirect('/');
+        else 
+            console.log("Error while creating new post");
+    });
 });
 
 server.listen(process.env.PORT, function() {
